f5e4ac328f9cf88bd90ca9b08832a63cfef88821
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-10-06 13:41:31 +0530

    fix  :  oh i forgot to remove the header :(

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import Footer from "./_components/Footer";
 export default function Home() {
   return (
     <main className="flex flex-col bg-gradient-to-r from-pink-500/30 to-sky-500/30 dark:bg-blue-950">
-      <Header/>
       <Hero/>
       <Features/>
       <Intro/>
